62a8e3026a526e588b0ae25383e78d217481be77
islemhdd <hddislem00@gmail.com>
2025-06-14 23:24:55 -0100

    recreating the main

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('api')->get('/test', function (Request $request) {
-    return response()->json(['message' => 'API is working']);
-});
