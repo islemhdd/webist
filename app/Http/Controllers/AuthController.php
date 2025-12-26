b99dcd1d20e04120e63ed6f4e4112428fd51bb38
islemHdd <hddislem00@gmail.com>
2025-12-26 16:56:34 +0100

    sgmt\login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
 {
     public function index()
     {
-        return view('infermerie.login');
+        return view('brigade.login');
     }
 
     public function login(Request $request)
